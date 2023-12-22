886f72f0a8b01415c43da907541794d4585bcdae
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2023-12-22 15:34:49 +0100

    modification animation écriture

--- js/header.js
+++ js/header.js
@@ -7,27 +7,41 @@ header.addEventListener("click", () => {
 
 // -------------TEXT APPARAIT FUR ET A MESURE----------
 const titleElement = document.getElementById('animatedTitle');
-        const text = "Bonjour et bienvenue sur mon Portfolio";
+const text = "Bonjour et bienvenue sur mon Portfolio";
 
-        function animateTitle() {
-            let index = 0;
+function animateTitle() {
+    let index = 0;
 
-            function addLetter() {
-                titleElement.textContent = text.slice(0, index);
-                index++;
+    function addLetter() {
+        titleElement.textContent = text.slice(0, index);
+        index++;
 
-                if (index <= text.length) {
-                    setTimeout(addLetter, Math.floor(Math.random() * (150 - 80 )) + 80) // ajustez la durée ici (en millisecondes)
-                } else {
-                    // La boucle est terminée, vous pouvez choisir de répéter ou faire autre chose ici
-                    setTimeout(() => {
-                        index = 0;
-                        animateTitle();
-                    }, 3000); // temps d'attente avant la répétition (en millisecondes)
-                }
-            }
-
-            addLetter();
+        if (index <= text.length) {
+            setTimeout(addLetter, Math.floor(Math.random() * (150 - 80 )) + 80) // Ajoute une lettre entre 150 et 80 miliseconde
+        } else {
+            // La boucle est terminé et on rappelle la fonction
+            setTimeout(() => {
+                delLetter()
+            }, 1500);
         }
+    }
 
-        animateTitle();
+    function delLetter(){
+        titleElement.textContent = text.slice(0, index);
+        index--;
+
+        if (index >= 0) {
+            setTimeout(delLetter, Math.floor(Math.random() * (150 - 80 )) + 80) // Ajoute une lettre entre 150 et 80 miliseconde
+        } else {
+            // La boucle est terminé et on rappelle la fonction animation
+            setTimeout(() => {
+                index = 0;
+                animateTitle();
+            }, 1500); // temps d'attente avant la répétition (en millisecondes)
+        }
+    }
+
+    addLetter();
+}
+
+animateTitle();
