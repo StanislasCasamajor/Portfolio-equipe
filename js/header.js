81d146e815a13218c67d47c1eea507e3e19a81eb
Delhumeur <126495668+Delhumeur@users.noreply.github.com>
2023-12-22 15:15:44 +0100

    ajout titre anim

--- js/header.js
+++ js/header.js
@@ -3,3 +3,31 @@ header.addEventListener("click", () => {
     header.classList.toggle("navOver")
 });
 
+
+
+// -------------TEXT APPARAIT FUR ET A MESURE----------
+const titleElement = document.getElementById('animatedTitle');
+        const text = "Bonjour et bienvenue sur mon Portfolio";
+
+        function animateTitle() {
+            let index = 0;
+
+            function addLetter() {
+                titleElement.textContent = text.slice(0, index);
+                index++;
+
+                if (index <= text.length) {
+                    setTimeout(addLetter, Math.floor(Math.random() * (150 - 80 )) + 80) // ajustez la durée ici (en millisecondes)
+                } else {
+                    // La boucle est terminée, vous pouvez choisir de répéter ou faire autre chose ici
+                    setTimeout(() => {
+                        index = 0;
+                        animateTitle();
+                    }, 3000); // temps d'attente avant la répétition (en millisecondes)
+                }
+            }
+
+            addLetter();
+        }
+
+        animateTitle();
